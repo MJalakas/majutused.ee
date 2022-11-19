50b0861667980581788619031200c1240f4954a8
Marten <marten@jalakas.ee>
2022-11-19 17:29:18 +0200

    added Card & Header components. Removed deleted imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          strapi + react on zone XD
-        </p>
-
+        <p>strapi + react on zone XD</p>
       </header>
     </div>
   );
