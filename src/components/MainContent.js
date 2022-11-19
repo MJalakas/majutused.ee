2edd48d32d1ffebebbc252c396e87c0c9ca5447d
Marten <marten@jalakas.ee>
2022-11-19 21:42:12 +0200

    made Card component pull data dynamically from cardData.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "./Card";
+import data from "../cardData.js";
 
 export default function MainContent() {
-  return (
-    <div className="main-content">
-      <Card />
-    </div>
-  );
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
+  return <div className="main-content">{cards}</div>;
 }
